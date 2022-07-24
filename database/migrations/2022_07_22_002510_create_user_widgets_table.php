235b62ee74824e80e8bec14b4618d09c8e0be95b
root <root@ovz6.bubaololo.gmzem.vps.myjino.ru>
2022-07-24 19:13:03 +0000

    db scaffolded

---
+++ database/migrations/2022_07_22_002510_create_user_widgets_table.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_widgets', function (Blueprint $table) {
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Widget::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_widgets');
+    }
+};
